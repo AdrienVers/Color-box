c17ce77dcfec9b652bf19565ec7eeaf9d4b15685
AdrienVers <adrienvx77@outlook.fr>
2022-10-03 14:41:15 +0200

    Ajout d'un text sur la color-box

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 console.log("connecté !");
 
 const box = document.querySelector(".color-box");
+const resultat = document.querySelector(".color-resultat");
 
 box.addEventListener("click", () => {
   if (box.style.backgroundColor === "green") {
     box.style.backgroundColor = "red";
+    resultat.innerHTML = "Rouge";
   } else {
     box.style.backgroundColor = "green";
+    resultat.innerHTML = "Vert";
   }
 });
